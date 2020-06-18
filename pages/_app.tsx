34c42094e8dc46574e5aac55b74fda5739eed6c3
gegehprast <gegeh.prast@gmail.com>
2020-06-18 22:39:45 +0800

    loading

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import { AppProps } from 'next/app'
 import '../styles/ubuntu.css'
 import '../styles/tailwind.css'
+import '../styles/loading.css'
 
 const MyApp = ({ Component, pageProps }: AppProps): JSX.Element => {
     return <Component {...pageProps} />
