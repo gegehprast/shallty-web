be12669bd44fd15879dba3cfbb7e4104fe3eca64
gegehprast <gegeh.prast@gmail.com>
2020-06-21 03:05:17 +0800

    add ReactToastify

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,8 @@
 import { AppProps } from 'next/app'
+import 'react-toastify/dist/ReactToastify.min.css'
 import '../styles/ubuntu.css'
 import '../styles/tailwind.css'
+import '../styles/toastify.css'
 import '../styles/loading.css'
 
 const MyApp = ({ Component, pageProps }: AppProps): JSX.Element => {
