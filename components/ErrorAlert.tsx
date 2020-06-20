df4b9a1a5c7e9828c58cf7cd6090b83e1b4c4c8e
gegehprast <gegeh.prast@gmail.com>
2020-06-21 03:06:39 +0800

    toastify alert omponents

--- components/ErrorAlert.tsx
+++ components/ErrorAlert.tsx
@@ -1,16 +1,14 @@
 import ParserError from './ParserError'
 
-const ErrorAlert = ({ errorMessage }: { errorMessage: string }): JSX.Element => {
+const ErrorAlert = ({ message }: { message: string }): JSX.Element => {
     return (
-        <div className="w-full">
-            <div className="px-2 py-1 mt-4 text-lg text-left text-red-700 bg-red-100 border-l-4 border-red-500" role="alert">
-                <p className="text-sm font-bold md:text-base">
-                    <ParserError />
-                </p>
-                <span className="w-full text-sm md:text-base">
-                    {errorMessage}
-                </span>
-            </div>
+        <div className="w-full p-2 text-sm text-left md:text-base" role="alert">
+            <p className="text-sm font-bold md:text-base">
+                <ParserError />
+            </p>
+            <span className="w-full text-sm md:text-base">
+                {message.replace('Something went wrong. Error: ', '')}
+            </span>
         </div>
     )
 }
--- components/SuccessAlert.tsx
+++ components/SuccessAlert.tsx
@@ -1,15 +1,13 @@
-const SuccessAlert = ({ handleReparse }: { handleReparse: (e: React.MouseEvent) => void }): JSX.Element => {
+const SuccessAlert = ({ handleReparse }: { handleReparse: () => void }): JSX.Element => {
     return (
-        <div className="w-full">
-            <div className="px-2 py-1 mt-4 text-sm text-left text-blue-700 bg-blue-100 border-l-4 border-blue-500 md:text-base" role="alert">
-                <p className="font-bold">Kena shortlink lagi?</p>
-                <a href="#"
-                    className="text-sh-300 hover:text-sh-100"
-                    onClick={handleReparse}
-                >
-                    Klik di sini untuk memulai lagi.
-                </a>
-            </div>
+        <div className="w-full p-2 text-sm text-left md:text-base" role="alert">
+            <p className="mb-1 text-lg font-bold">Kena shortlink lagi?</p>
+            <button 
+                className="font-bold text-sh-300 hover:text-sh-100"
+                onClick={handleReparse}
+            >
+                Klik di sini untuk memulai lagi.
+            </button>
         </div>
     )
 }
