272029b15733d5d67968db6754d6e05eebb592e0
gegehprast <gegeh.prast@gmail.com>
2020-06-21 21:44:43 +0800

    help link

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import Link from 'next/link'
 import Parser from '../components/Parser'
 import Footer from '../components/Footer'
 import { ToastContainer } from 'react-toastify'
@@ -54,9 +55,11 @@ const Home = (): JSX.Element => {
                                     <path d="M2.93 17.07A10 10 0 1 1 17.07 2.93 10 10 0 0 1 2.93 17.07zm12.73-1.41A8 8 0 1 0 4.34 4.34a8 8 0 0 0 11.32 11.32zM9 5h2v6H9V5zm0 8h2v2H9v-2z" />
                                 </svg>
                                 &nbsp;
-                                <a href="/help" className="sh-text-shadow">
-                                    Bantuan
-                                </a>
+                                <Link href="/help" as="/help">
+                                    <a className="sh-text-shadow">
+                                        Bantuan
+                                    </a>
+                                </Link>
                             </div>
                         </div>
                     </main>
