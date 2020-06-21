5d435e51823819f01ec542728d99a64a403161a7
gegehprast <gegeh.prast@gmail.com>
2020-06-21 22:29:30 +0800

    update footer browser extension link

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,6 +1,17 @@
 import React from 'react'
+import { showToast, dismissToast } from '../utils/toast'
 
 const Footer = (): JSX.Element => {
+    const handleClick = (e: React.MouseEvent) => {
+        e.preventDefault()
+
+        dismissToast('IN_DEVELOPMENT')
+
+        showToast(<div className="p-2 text-lg">Sedang dikembangkan.</div>, {
+            id: 'IN_DEVELOPMENT',
+            type: 'info',
+        })
+    }
     return (
         <div className="relative flex flex-row flex-wrap items-center justify-center w-screen mx-auto text-gray-400">
             <div className="flex flex-wrap items-center justify-around w-full px-2 mt-auto mb-8 md:px-8 md:w-11/12 lg:w-5/6 xl:w-1/2">
@@ -11,8 +22,8 @@ const Footer = (): JSX.Element => {
                     <a href="http://bit.ly/ShalltyDiscordServer" className="truncate transition-colors duration-200 ease-in text-sh-300 hover:text-sh-100" target="_blank" rel="noreferrer">
                         Discord
                     </a>
-                    <a href="http://bit.ly/ShalltyDiscordServer" className="ml-4 truncate transition-colors duration-200 ease-in text-sh-300 hover:text-sh-100" target="_blank" rel="noreferrer">
-                        Ekstensi Chrome
+                    <a href="#" onClick={handleClick} className="ml-4 truncate transition-colors duration-200 ease-in text-sh-300 hover:text-sh-100" target="_blank" rel="noreferrer">
+                        Browser Extension
                     </a>
                     <a href="https://github.com/gegehprast/shallty" className="ml-4 truncate transition-colors duration-200 ease-in text-sh-300 hover:text-sh-100" target="_blank" rel="noreferrer">
                         Open Source
