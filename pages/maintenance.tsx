81a87bc8ab559c3097bf8a3c1cb21a5ce7d28131
gegehprast <gegeh.prast@gmail.com>
2020-06-22 01:01:50 +0800

    maintenance mode

--- pages/maintenance.tsx
+++ pages/maintenance.tsx
@@ -1,33 +1,21 @@
 
 import Link from 'next/link'
-import Parser from '../components/Parser'
 import Footer from '../components/Footer'
 import { ToastContainer } from 'react-toastify'
 import MetaHead from '../components/MetaHead'
-import { useRouter } from 'next/router'
-import { maintenance } from '../config.json'
-import { useEffect } from 'react'
 
-const Home = (): JSX.Element => {
-    const router = useRouter()
-
-    useEffect(() => {
-        if (maintenance === true) {
-            router.push('/maintenance')
-        }
-    }, [router])
-    
+const Maintenance = (): JSX.Element => {
     return (
         <>
             <MetaHead />
-            
-            <div className="relative flex flex-wrap w-full h-screen bg-center bg-no-repeat bg-cover bg-y-35" 
+
+            <div className="relative flex flex-wrap w-full h-screen bg-center bg-no-repeat bg-cover bg-y-35"
                 style={{ backgroundColor: '#d49397' }}
             >
-                <img src="/bg.png" className="absolute object-cover w-full h-screen" 
-                    style={{ objectPosition: '50% 25%', filter: 'blur(2px)', WebkitFilter: 'blur(2px)' }} 
+                <img src="/bg.png" className="absolute object-cover w-full h-screen"
+                    style={{ objectPosition: '50% 25%', filter: 'blur(2px)', WebkitFilter: 'blur(2px)' }}
                 />
-                
+
                 <div className="absolute w-full h-screen main-overlay"></div>
 
                 <div className="relative w-full p-8 mx-auto text-center text-white">
@@ -41,11 +29,10 @@ const Home = (): JSX.Element => {
                 </div>
 
                 <div className="relative w-full text-white">
-
                     <main className="px-2 mx-auto md:px-8 md:w-11/12 lg:w-5/6 xl:w-1/2">
-                        <Parser />
-
-                        
+                        <h1 className="text-5xl font-bold md:text-6xl sh-text-shadow">
+                            Oof! Sedang maintenance.
+                        </h1>
                     </main>
                 </div>
 
@@ -57,4 +44,4 @@ const Home = (): JSX.Element => {
     )
 }
 
-export default Home
+export default Maintenance
