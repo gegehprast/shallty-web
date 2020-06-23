c2099875196e39eb723b50941913f4d9832b697b
gegehprast <gegeh.prast@gmail.com>
2020-06-23 14:09:15 +0800

    0.14.0

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -21,7 +21,7 @@ const Footer = ({ textClass, linkClass }: Props): JSX.Element => {
         <div className="relative flex flex-row justify-center w-screen mx-auto text-sm text-gray-400 md:text-base">
             <div className="flex flex-wrap items-center justify-around w-full px-2 mt-auto mb-8 md:px-8 md:w-11/12 lg:w-5/6 xl:w-1/2">
                 <div className={`w-full mt-4 md:w-1/2 md:mt-0 ${textClass}`}>
-                    © 2020 shallty.moe FC-v0.9
+                    © 2020 shallty.moe {process.env.NEXT_PUBLIC_APP_VERSION}
                 </div>
                 <div className="order-first w-full md:text-right md:order-none md:w-1/2">
                     <a href="http://bit.ly/ShalltyDiscordServer" className={`truncate transition-colors duration-200 ease-in text-sh-300 hover:text-sh-100 ${linkClass}`} target="_blank" rel="noreferrer">
