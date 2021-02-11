6d75dcf41cf3a27a1f742a7a20b93c8d35ac1d9f
gegehprast <gegeh.prast@gmail.com>
2021-02-11 19:52:54 +0700

    tired

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import { showToast, dismissToast } from '../utils/toast'
 
 interface Props {
     textClass?: string
@@ -7,16 +6,6 @@ interface Props {
 }
 
 const Footer = ({ textClass, linkClass }: Props): JSX.Element => {
-    const handleClick = (e: React.MouseEvent) => {
-        e.preventDefault()
-
-        dismissToast('IN_DEVELOPMENT')
-
-        showToast(<div className="p-2 text-lg">Mungkin nanti.</div>, {
-            id: 'IN_DEVELOPMENT',
-            type: 'info',
-        })
-    }
     return (
         <div className="relative flex flex-row justify-center w-screen mx-auto text-sm text-gray-400 md:text-base">
             <div className="flex flex-wrap items-center justify-around w-full px-2 mt-auto mb-8 md:px-8 md:w-11/12 lg:w-5/6 xl:w-1/2">
@@ -26,9 +15,6 @@ const Footer = ({ textClass, linkClass }: Props): JSX.Element => {
                 <div className="order-first w-full md:text-right md:order-none md:w-1/2">
                     <a href="http://bit.ly/ShalltyDiscordServer" className={`truncate transition-colors duration-200 ease-in text-sh-300 hover:text-sh-100 ${linkClass}`} target="_blank" rel="noreferrer">
                         Discord
-                    </a>
-                    <a href="#" onClick={handleClick} className={`ml-4 truncate transition-colors duration-200 ease-in text-sh-300 hover:text-sh-100 ${linkClass}`} target="_blank" rel="noreferrer">
-                        Browser Extension
                     </a>
                     <a href="https://github.com/gegehprast/shallty" className={`ml-4 truncate transition-colors duration-200 ease-in text-sh-300 hover:text-sh-100 ${linkClass}`} target="_blank" rel="noreferrer">
                         Open Source
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,5 @@
 
 import Link from 'next/link'
-import Parser from '../components/Parser'
 import Footer from '../components/Footer'
 import { ToastContainer } from 'react-toastify'
 import MetaHead from '../components/MetaHead'
@@ -16,18 +15,18 @@ const Home = (): JSX.Element => {
             router.push('/maintenance')
         }
     }, [router])
-    
+
     return (
         <>
             <MetaHead />
-            
-            <div className="relative flex flex-wrap w-full h-screen bg-center bg-no-repeat bg-cover bg-y-35" 
+
+            <div className="relative flex flex-wrap w-full h-screen bg-center bg-no-repeat bg-cover bg-y-35"
                 style={{ backgroundColor: '#d49397' }}
             >
-                <img src="/bg.png" className="absolute object-cover w-full h-screen" 
-                    style={{ objectPosition: '50% 25%', filter: 'blur(2px)', WebkitFilter: 'blur(2px)' }} 
+                <img src="/bg.png" className="absolute object-cover w-full h-screen"
+                    style={{ objectPosition: '50% 25%', filter: 'blur(2px)', WebkitFilter: 'blur(2px)' }}
                 />
-                
+
                 <div className="absolute w-full h-screen main-overlay"></div>
 
                 <div className="relative w-full p-8 mx-auto text-center text-white">
@@ -43,9 +42,15 @@ const Home = (): JSX.Element => {
                 <div className="relative w-full text-white">
 
                     <main className="px-2 mx-auto md:px-8 md:w-11/12 lg:w-5/6 xl:w-1/2">
-                        <Parser />
+                        <div className="flex flex-col justify-center w-full">
+                            <div className="text-3xl font-semibold text-center md:text-6xl sh-text-shadow">
+                                Shallty is a bit tired.
+                            </div>
 
-                        
+                            <div className="mt-4 text-lg font-semibold text-center md:text-4xl sh-text-shadow">
+                                Come back later. Maybe.
+                            </div>
+                        </div>
                     </main>
                 </div>
 
